175dc1cfe62fd36948dfa62f965b5b252c3922e0
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-11-30 01:08:23 +0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
 const bot = new Telegraf('649515347:AAGFcnAi7olLsuxlZa4aQHesCkWcdxCmYAs');
 bot.start((ctx) => ctx.reply('Welcome'));
+bot.command('register', ({ reply }) =>
+    reply('we are glad to have u as contributor', Markup
+        .keyboard(['/simple', '/inline', '/pyramid'])
+        .oneTime()
+        .resize()
+        .extra()
+    )
+)
 bot.on('text', (ctx) => {
     return ctx.reply('This city is not exists');
 });
