5227f3090da47019461d5e73cd8e77065aa52071
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-11-30 01:16:20 +0300

    fix

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
 const bot = new Telegraf('649515347:AAGFcnAi7olLsuxlZa4aQHesCkWcdxCmYAs');
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.command('register', ({ reply }) =>
-    reply('we are glad to have u as contributor', Markup
-        .keyboard(['/simple', '/inline', '/pyramid'])
+bot.command('start', ({ reply }) =>
+    reply('welcome! \n we are glad to have u as contributor', Markup
+        .keyboard(['/register'])
         .oneTime()
         .resize()
         .extra()
     )
-)
+);
+bot.command('register', ({reply}) =>
+    reply('Enter your location',
+        Markup.keyboard([{text: 'My Location', request_location: true}])
+            .oneTime()
+            .resize()
+            .extra()
+    )
+);
 bot.on('text', (ctx) => {
     return ctx.reply('This city is not exists');
 });
