30a10c4d18b4fcab168668dbef6a841abb8532c5
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-12-01 00:32:56 +0300

    updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bot = new TelegramBot(TOKEN,  {polling: true});
 const url = 'https://adrashye.herokuapp.com:443';
 const firebase = require('./lib/firebase');
 const Firebase = new firebase.Firebase();
-bot.setWebHook(`${url}/bot${TOKEN}`);
+
 
 bot.on('message', message(bot, {}, firebase.Firebase));
 bot.on('callback_query', callbackQuery(bot, {}, firebase.Firebase));
