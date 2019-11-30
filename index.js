63aa0d11b33bd9d2d75f5855f2acd05fef459dfa
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-11-30 12:47:40 +0300

    fix

--- index.js
+++ index.js
@@ -5,42 +5,18 @@ const session = require('telegraf/session');
 const Stage = require('telegraf/stage');
 const Scene = require('telegraf/scenes/base');
 const { leave } = Stage
-
-/*bot.command('register', (ctx) => {
-    ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup((m) =>
-        m.keyboard([
-            m.locationRequestButton('Send 📍'),
-        ]).oneTime(false)
-    )).then((msg) => {
-        return ctx.reply(String(msg.location.latitude))
-    }, () => ctx.reply(JSON.stringify(msg)))
-});
-bot.action('Register',  (ctx, next) => {
-  return ctx.reply('👍 hi ' + ctx.longitude).then(next)
-});*/
-
-// Greeter scene
-const greeter = new Scene('registration');
-greeter.enter((ctx) =>     ctx.replyWithHTML(`welcome <b>${ctx.from.first_name}!</b> \nwe are glad to have u as contributor`,
-    Extra.HTML().markup((m) =>
-        m.keyboard([
-            m.locationRequestButton('Send 📍'),
-        ]).oneTime(false)
-    )));
-greeter.on('message', (ctx) => ctx.reply('Send hi' + JSON.stringify(ctx)));
-
-// Create scene manager
-const stage = new Stage();
-stage.command('cancel', leave());
-
-// Scene registration
-stage.register(greeter);
 const bot = new Telegraf('649515347:AAGFcnAi7olLsuxlZa4aQHesCkWcdxCmYAs');
-bot.use(session());
-bot.use(stage.middleware());
-bot.command('register', (ctx) => ctx.scene.enter('registration'))
-bot.startPolling()
-bot.on('text', (ctx) => {
-    return ctx.reply(JSON.stringify(ctx));
+bot.command('start', ( ctx ) =>
+    ctx.replyWithHTML(`welcome <b>${ctx.from.first_name}!</b> \nwe are glad to have u as contributor`)
+);
+bot.command('register', (ctx) => {
+    return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup((m) =>
+        m.keyboard([
+            m.locationRequestButton('Send 📍'),
+        ]).oneTime(false)
+    ))
+});
+bot.on('message', (ctx) => {
+    return ctx.reply(String(ctx));
 });
 bot.launch();
