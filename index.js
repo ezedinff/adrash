df468a5376405e41182e567c3690668e83350940
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-11-30 21:02:12 +0300

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const message = require('./responders/message');
 const callbackQuery = require('./responders/callback');
 
 const TOKEN = '649515347:AAGFcnAi7olLsuxlZa4aQHesCkWcdxCmYAs';
-const bot = new TelegramBot(TOKEN);
+const bot = new TelegramBot(TOKEN,  {polling: true});
 const url = 'https://adrashye.herokuapp.com:443';
 const firebase = require('./lib/firebase');
 
