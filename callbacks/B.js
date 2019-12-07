20db90e5dbc4635783f3c749fb25c8a1acea1f7c
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-12-07 23:39:58 +0300

    fix

--- callbacks/B.js
+++ callbacks/B.js
@@ -6,17 +6,29 @@ module.exports = (config, bot, callbackQuery, firebase) => {
   const cqBadNooice = () => {
     bot.answerCallbackQuery(callbackQuery.id, 'NOOICE?', false);
   };
-    firebase.addData({
-        location: {
-            type: 'point',
-            coordinates: [data.l[0], data.l[1]]
-        },
-        name: data.n,
-        contributor: data.cid,
-        approved: false
-    });
-    bot.sendMessage(data.chatId, 'ቦታው በተሳካ ሁኔታ ተመዝግቡዋል!!\nአመሰግናለው 🙌🏿', {
-        reply_to_message_id: data.mid,
-    });
+    if (!data.n.startsWith('/')){
+        firebase.addData({
+            location: {
+                type: 'point',
+                coordinates: [data.l[0], data.l[1]]
+            },
+            name: data.n,
+            contributor: data.cid,
+            approved: false
+        });
+        bot.sendMessage(data.chatId, 'ቦታው በተሳካ ሁኔታ ተመዝግቡዋል!!\nአመሰግናለው 🙌🏿', {
+            reply_to_message_id: data.mid,
+        });
+    } else {
+        bot.sendMessage(msg.chat.id, 'you\'re already a contributor 🙌🏿\n\nPS\nTo unregister send /unregister command', {
+            reply_markup: JSON.stringify({
+                keyboard: [
+                    [{ text: 'Send 📍', request_location: true }],
+                ],
+                resize_keyboard: true,
+                one_time_keyboard: false,
+            }),
+        });
+    }
     return;
 };
