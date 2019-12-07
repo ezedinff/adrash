898c23341f351fe27c0b542c5f78ee08550f0739
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-12-07 22:27:32 +0300

    fix

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const TelegramBot = require('node-telegram-bot-api');
 const message = require('./responders/message');
 const callbackQuery = require('./responders/callback');
-
+const options = {
+    webHook: {
+        port: process.env.PORT,
+    },
+};
 const TOKEN = '649515347:AAGFcnAi7olLsuxlZa4aQHesCkWcdxCmYAs';
-const bot = new TelegramBot(TOKEN,  {polling: true});
+const bot = new TelegramBot(TOKEN,  options);
 const url = 'https://adrashye.herokuapp.com:443';
 const firebase = require('./lib/firebase');
 const Firebase = new firebase.Firebase();
