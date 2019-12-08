d522e79344940370ef7bb7efbd489bf2a4cfac7d
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-12-08 22:26:30 +0300

    fix

--- callbacks/A.js
+++ callbacks/A.js
@@ -7,20 +7,22 @@ module.exports = async (config, bot, callbackQuery, firebase, cb) => {
     bot.answerCallbackQuery(callbackQuery.id, 'NOOICE!', false);
     bot.sendMessage(callbackQuery.message.chat.id, 'በዚህ መሠረት ይላኩልን\n' +
         'የታክሲ መያዢያው ቦታስም - የምን ታክሲ እየያዙ እንዳሉ', {
-        reply_to_message_id: callbackQuery.message.reply_to_message.message_id,
+        reply_to_message_id: callbackQuery.message.reply_to_message.message_id
+    }).then((payload) => {
+        bot.once('message', (msg) => {
+            if (msg && msg.text !== '😇 Register a Place') {
+                const t = msg.text ? msg.text : '';
+                if (t.split('-').length < 2) {
+                    bot.sendMessage(callbackQuery.message.chat.id, 'እባክዎን በድጋሚ በዚህ መሠረት ይላኩልን\n' +
+                        'የታክሲ መያዢያው ቦታስም - የምን ታክሲ እየያዙ እንዳሉ', {
+                        reply_to_message_id: callbackQuery.message.reply_to_message.message_id,
+                    });
+                } else {
+                    bot.on('message', (msg) => {
+                        cb({data:  JSON.stringify({chatId: msg.chat.id, mid: msg.message_id, l: data.l, n: msg.text, cid: msg.from.id})})
+                    });
+                }
+            }
+        });
     });
-    var msg = callbackQuery.message;
-    if (msg && msg.text !== '😇 Register a Place') {
-        const t = msg.text ? msg.text : '';
-        if (t.split('-').length < 2) {
-            bot.sendMessage(callbackQuery.message.chat.id, 'እባክዎን በድጋሚ በዚህ መሠረት ይላኩልን\n' +
-                'የታክሲ መያዢያው ቦታስም - የምን ታክሲ እየያዙ እንዳሉ', {
-                reply_to_message_id: callbackQuery.message.reply_to_message.message_id,
-            });
-        } else {
-            bot.on('message', (msg) => {
-                cb({data:  JSON.stringify({chatId: msg.chat.id, mid: msg.message_id, l: data.l, n: msg.text, cid: msg.from.id})})
-            });
-        }
-    }
 };
