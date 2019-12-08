46156a2d915b6ee8950317d40f9812490575ab36
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-12-08 21:55:56 +0300

    fix

--- callbacks/A.js
+++ callbacks/A.js
@@ -13,13 +13,14 @@ module.exports = async (config, bot, callbackQuery, firebase, cb) => {
     if (msg) {
         const t = msg.text ? msg.text : '';
         if (t.split('-').length < 2) {
-            bot.sendMessage(callbackQuery.message.chat.id, 'በዚህ መሠረት ይላኩልን\n' +
+            bot.sendMessage(callbackQuery.message.chat.id, 'እባክዎን በድጋሚ በዚህ መሠረት ይላኩልን\n' +
                 'የታክሲ መያዢያው ቦታስም - የምን ታክሲ እየያዙ እንዳሉ', {
                 reply_to_message_id: callbackQuery.message.reply_to_message.message_id,
             });
+        } else {
+            bot.on('message', (msg) => {
+                cb({data:  JSON.stringify({chatId: msg.chat.id, mid: msg.message_id, l: data.l, n: msg.text, cid: msg.from.id})})
+            });
         }
-        bot.on('message', (msg) => {
-            cb({data:  JSON.stringify({chatId: msg.chat.id, mid: msg.message_id, l: data.l, n: msg.text, cid: msg.from.id})})
-        });
     }
 };
