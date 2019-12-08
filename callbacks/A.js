81c361f8ef9bbdd004977a3782d68c88c3ba6d7d
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-12-09 01:06:29 +0300

    fix

--- callbacks/A.js
+++ callbacks/A.js
@@ -7,9 +7,13 @@ module.exports = async (config, bot, callbackQuery, firebase, cb) => {
     bot.answerCallbackQuery(callbackQuery.id, 'NOOICE!', false);
     bot.sendMessage(callbackQuery.message.chat.id, 'በዚህ መሠረት ይላኩልን\n' +
         'የታክሲ መያዢያው ቦታስም - የምን ታክሲ እየያዙ እንዳሉ', {
-        reply_to_message_id: callbackQuery.message.reply_to_message.message_id
+        reply_to_message_id: callbackQuery.message.reply_to_message.message_id,
+        reply_markup: {
+            force_reply: true
+        }
     }).then((payload) => {
-        bot.once('message', (msg) => {
+        const replyListenerId = bot.onReplyToMessage(payload.chat.id, payload.message_id, msg => {
+            bot.removeReplyListener(replyListenerId);
             if (msg && msg.text !== '😇 Register a Place') {
                 const t = msg.text ? msg.text : '';
                 console.log(t);
@@ -28,5 +32,24 @@ module.exports = async (config, bot, callbackQuery, firebase, cb) => {
                 }
             }
         });
+/*        bot.once('message', (msg) => {
+            if (msg && msg.text !== '😇 Register a Place') {
+                const t = msg.text ? msg.text : '';
+                console.log(t);
+                if (t.split('-').length < 2) {
+                    bot.sendMessage(msg.chat.id, 'እባክዎን በድጋሚ በዚህ መሠረት ይላኩልን\n' +
+                        'የታክሲ መያዢያው ቦታስም - የምን ታክሲ እየያዙ እንዳሉ', {
+                        reply_to_message_id: msg.message_id,
+                        reply_markup: JSON.stringify({
+                            inline_keyboard: [
+                                [{ text: '😇 Register a Place', callback_data: JSON.stringify({ type: 'A', l: data.l }) }],
+                            ],
+                        }),
+                    });
+                } else {
+                    cb({data:  JSON.stringify({chatId: msg.chat.id, mid: msg.message_id, l: data.l, n: msg.text, cid: msg.from.id})})
+                }
+            }
+        });*/
     });
 };
