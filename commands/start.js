e573f0880b521cc3d40ad06e9e11472f6ee74476
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-12-08 23:17:17 +0300

    fix

--- commands/start.js
+++ commands/start.js
@@ -1,13 +1,12 @@
 module.exports = (bot, msg, firebase) => {
     console.log(msg);
-    bot.sendMessage(msg.chat.id, `*እንኳን ደህና መጣህ ${msg.from.first_name}*!
-    to register /register
-PS
-Turn on your Wi-Fi to have better accuracy
-
-PPS
-To register please 🙏🏿 make sure your GPS accuracy is within *20 meters*`, {
-        parse_mode: 'Markdown',
-        disable_web_page_preview: true,
+    bot.sendMessage(msg.chat.id ? msg.chat.id : msg.from.id, 'You are now a contributor 🙌🏿\n\nWhenever you send me your location I\'ll ask you if you want to register a place. \n\n to send me your location please click the Send Location button.', {
+        reply_markup: JSON.stringify({
+            keyboard: [
+                [{ text: 'Send Location 📍', request_location: true }],
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: false,
+        }),
     });
 };
