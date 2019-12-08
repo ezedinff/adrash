4cb2422315fee06b81c68bf85205413b9010c647
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2019-12-08 10:04:00 +0300

    fix

--- commands/register.js
+++ commands/register.js
@@ -1,26 +1,13 @@
 module.exports = (bot, msg, firebase) => {
   bot.sendChatAction(msg.chat.id, 'typing');
-  console.log(msg);
-  firebase.addContributor(msg.from.id, msg.from)
-    .then(() => {
-      bot.sendMessage(msg.chat.id ? msg.chat.id : msg.from.id, 'You are now a contributor 🙌🏿\n\nWhenever you send me your location I\'ll ask you if you want to register a place', {
+  firebase.addContributor(msg.from.id, msg.from);
+  bot.sendMessage(msg.chat.id ? msg.chat.id : msg.from.id, 'You are now a contributor 🙌🏿\n\nWhenever you send me your location I\'ll ask you if you want to register a place. \n\n to send me your location please click the Send Location button.', {
         reply_markup: JSON.stringify({
-          keyboard: [
-            [{ text: 'Send 📍', request_location: true }],
-          ],
-          resize_keyboard: true,
-          one_time_keyboard: false,
+            keyboard: [
+                [{ text: 'Send Location 📍', request_location: true }],
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: false,
         }),
-      });
-    }, () => {
-      bot.sendMessage(msg.chat.id ? msg.chat.id : msg.from.id, 'you\'re already a contributor 🙌🏿\n\nPS\nTo unregister send /unregister command', {
-        reply_markup: JSON.stringify({
-          keyboard: [
-            [{ text: 'Send 📍', request_location: true }],
-          ],
-          resize_keyboard: true,
-          one_time_keyboard: false,
-        }),
-      });
     });
 };
