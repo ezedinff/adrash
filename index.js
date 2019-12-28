295f3c6b5c104b1ce1535740c0649cf86a49593e
Ezedin <ezedin.fedlu@gmail.com>
2019-12-28 10:00:46 +0300

    fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const message = require('./responders/message');
 const callbackQuery = require('./responders/callback');
+var CronJob = require('cron').CronJob;
 const options = {
     webHook: {
         port: process.env.PORT,
@@ -16,3 +17,31 @@ const Firebase = new firebase.Firebase();
 bot.setWebHook(`${url}/bot${TOKEN}`);
 bot.on('message', message(bot, [], firebase.Firebase));
 bot.on('callback_query', callbackQuery(bot, [], firebase.Firebase));
+function sendToAll (message) {
+    firebase.Firebase.contributors().then((snapshot) => {
+        snapshot.forEach(doc => {
+            const cont = doc.data();
+            bot.sendMessage(
+                doc.id,
+                `ሰላም ${cont.first_name}\n\n${message}`
+            ).then(r => console.log(r)).catch(err => console.log(err))
+        });
+    });
+  }
+ // send messages to all contributors every morning
+const job = new CronJob('00 58 9 * * 0-6', function() {
+    firebase.Firebase.getMessages().then((snapshot) => {
+            const documents = [];
+            snapshot.forEach(doc => {
+                const t = doc.data();
+                documents.push(t.message)
+            });
+            if (documents.length > 0) {
+                const message = documents[Math.floor(Math.random() * ((documents.length - 1) - 0 + 1) + 0)];
+                sendToAll(message);
+            }
+        })
+  });
+  job.start();
+
+  // 00 28 8 * * 1-6
--- responders/message.js
+++ responders/message.js
@@ -3,7 +3,7 @@ const approve = require('./../commands/approve');
 const disapprove = require('./../commands/disapprove');
 const ndelete = require('./../commands/delete');
 const register = require('./../commands/register');
-var CronJob = require('cron').CronJob;
+
 
 // all will happen inside a `message` - middleware will be applied
 // to break the monolithic crap here
@@ -24,7 +24,7 @@ module.exports = (bot, config, firebase) => (msg) => {
             const cont = doc.data();
             bot.sendMessage(
                 doc.id,
-                `ሰላም ${cont.first_name}\n\n${bM}`
+                `ሰላም ${cont.first_name}\n\n${message}`
             ).then(r => console.log(r)).catch(err => console.log(err))
         });
     });
@@ -45,21 +45,7 @@ module.exports = (bot, config, firebase) => (msg) => {
          );
       });
   }
-  // send messages to all contributors every morning
-  const job = new CronJob('00 30 8 * * 1-5', function() {
-        firebase.getMessages().then((snapshot) => {
-            const documents = [];
-            snapshot.forEach(doc => {
-                const t = doc.data();
-                documents.push(t.message)
-            });
-            if (documents.length > 0) {
-                const message = documents[Math.floor(Math.random() * ((documents.length - 1) - 0 + 1) + 0)];
-                sendToAll(message);
-            }
-        })
-  }, null, true, 'Africa/Nairobi');
-  job.start();
+
   // /start
   if (msg.text === '/start') {
     start(bot, msg, firebase);
