0906c50be1f0c490d94b95fd12ffc1a27e0091bf
Ezedin <ezedin.fedlu@gmail.com>
2019-12-28 05:46:21 +0300

    adding message and sending messages every morning

--- responders/message.js
+++ responders/message.js
@@ -3,6 +3,7 @@ const approve = require('./../commands/approve');
 const disapprove = require('./../commands/disapprove');
 const ndelete = require('./../commands/delete');
 const register = require('./../commands/register');
+var CronJob = require('cron').CronJob;
 
 // all will happen inside a `message` - middleware will be applied
 // to break the monolithic crap here
@@ -17,20 +18,45 @@ module.exports = (bot, config, firebase) => (msg) => {
           }),
       });
   }
+  function sendToAll (message) {
+    firebase.contributors().then((snapshot) => {
+        snapshot.forEach(doc => {
+            const cont = doc.data();
+            bot.sendMessage(
+                doc.id,
+                `ሰላም ${cont.first_name}\n\n${bM}`
+            ).then(r => console.log(r)).catch(err => console.log(err))
+        });
+    });
+  }
   const t = msg.text ? msg.text : '';
   if (t.startsWith('###71?')) {
-        firebase.contributors().then((snapshot) => {
-            const bM = msg.text.split('?')[1];
-            snapshot.forEach(doc => {
-                const cont = doc.data();
-                bot.sendMessage(
-                    doc.id,
-                    `ሰላም ${cont.first_name}\n\n${bM}`
-                ).then(r => console.log(r)).catch(err => console.log(err))
-            });
-        });
+    const bM = msg.text.split('?')[1];
+    sendToAll(bM);
   }
 
+  // add new message to database
+  if(t.startsWith('###72?')) {
+      var m = msg.text.split('?')[1];
+      firebase.addMessage({message: m}).then(() => {
+         bot.sendMessage(
+             msg.chat.id,
+             'message successfully registred!!'
+         );
+      });
+  }
+  // send messages to all contributors every morning
+  const job = new CronJob('00 30 8 * * 1-5', function() {
+        firebase.getMessages().then((snapshot) => {
+            const documents = [];
+            snapshot.forEach(doc => documents.push(doc.data()));
+            if (documents.length > 0) {
+                const message = documents[Math.floor(Math.random() * ((documents.length - 1) - 0 + 1) + 0)];
+                sendToAll(message);
+            }
+        })
+  }, null, true, 'Africa/Nairobi');
+  job.start();
   // /start
   if (msg.text === '/start') {
     start(bot, msg, firebase);
